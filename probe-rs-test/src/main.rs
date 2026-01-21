d32e99f6a52ac625ea9af017fb719545d26df470
Robin Mueller <muellerr@irs.uni-stuttgart.de>
2026-01-21 10:51:55 +0100

    weird stuff happening

--- probe-rs-test/src/main.rs
+++ probe-rs-test/src/main.rs
@@ -1,8 +1,9 @@
 use std::{path::Path, time::Duration};
 
+use anyhow::Context;
 use probe_rs::{
     Permissions,
-    architecture::arm::FullyQualifiedApAddress,
+    architecture::arm::{FullyQualifiedApAddress, core::registers::cortex_m::PC},
     flashing::{ElfOptions, Format, build_loader},
     probe::list::Lister,
     vendor::amd::sequences::x7z::AccessPort,
@@ -14,7 +15,6 @@ fn main() -> anyhow::Result<()> {
     let probe = probes[0].open()?;
     let mut session = probe.attach("X7Z", Permissions::default()).unwrap();
 
-    let ap_addr = FullyQualifiedApAddress::v1_with_default_dp(AccessPort::SystemMemory as u8);
     //mem_ap.
     let mut core = session.core(0)?;
     core.halt(Duration::from_millis(200))?;
@@ -31,6 +31,12 @@ fn main() -> anyhow::Result<()> {
     for segment in &raw_builder.data {
         println!("{} bytes of data at base {:?}", segment.1.len(), segment.0);
     }
+    let aps = session
+        .get_arm_interface()?
+        .access_ports(probe_rs::architecture::arm::dp::DpAddress::Default)?;
+    println!("APs: {:?}", aps);
+    // Using the debug memory here makes a later call to core(0) fail (????).
+    let ap_addr = FullyQualifiedApAddress::v1_with_default_dp(AccessPort::SystemMemory as u8);
     let mut mem_ap = session.get_arm_interface()?.memory_interface(&ap_addr)?;
     for segment in &raw_builder.data {
         println!(
@@ -41,9 +47,14 @@ fn main() -> anyhow::Result<()> {
         mem_ap.write(*segment.0, segment.1)?;
     }
     drop(mem_ap);
-    let mut core = session.core(0)?;
+
+    let mut core = session
+        .core(0)
+        .with_context(|| "retrieving core 0 failed")?;
     println!("running core");
-    core.run()?;
+    core.write_core_reg(PC.id, loader.vector_table_addr().unwrap())
+        .with_context(|| "setting PC")?;
+    core.run().with_context(|| "resuming core")?;
     loop {
         std::thread::sleep(Duration::from_millis(100));
     }
