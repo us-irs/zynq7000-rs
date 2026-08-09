87e240388f90a94a39741a618a4bbf6c4626d115
Robin Mueller <muellerr@irs.uni-stuttgart.de>
2026-08-10 00:21:04 +0200

    archive legacy stdgen workflow

--- zedboard-gateware/export-hw.tcl
+++ zedboard-gateware/export-hw.tcl
@@ -1,2 +1,6 @@
+set script_dir [file dirname [info script]]
+open_project $script_dir/zedboard-rust/zedboard-rust.xpr
+open_run impl_1
+
 set hw_file "[get_property DIRECTORY [current_project]]/zedboard-rust.xsa"
 write_hw_platform -fixed -include_bit -force -file $hw_file
